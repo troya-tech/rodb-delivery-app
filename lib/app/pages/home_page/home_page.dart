f0ddab4909df65ed0b613182b8a812e828b512e6
foorcun <msnifoorcun@hotmail.com>
2026-02-09 16:26:51 +0300

    temp save

--- lib/app/pages/home_page/home_page.dart
+++ lib/app/pages/home_page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../../features/restaurant-user-feature/presentation/user_profile_screen.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +7,21 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
+      appBar: AppBar(
+        title: const Text('Home'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const UserProfileScreen(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
       body: const Center(child: Text('Welcome Home!')),
     );
   }
